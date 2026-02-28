dc88d03620c01dcfd297ee7a2e8c131809de342a
Jose Angel Mate Carias <mc26002@esfe.agape.edu.sv>
2026-02-28 09:21:44 -0600

    Creacion de la carpeta de migraciones

--- Agro-Mercado.AppMVC/Program.cs
+++ Agro-Mercado.AppMVC/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Agro-Mercado.AppMVC.Models;
+using Agro_Mercado.AppMVC.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
