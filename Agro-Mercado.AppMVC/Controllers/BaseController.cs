f1e87df58900d656998a5c8596b06a6ba81568c0
Jose Angel Mate Carias <mc26002@esfe.agape.edu.sv>
2026-02-28 18:38:21 -0600

    Tarea: Validar campos obligatorios

---
+++ Agro-Mercado.AppMVC/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace Agro_Mercado.AppMVC.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected bool TieneAcceso(params int[] rolesPermitidos)
+        {
+            var rolId = HttpContext.Session.GetInt32("RolId");
+
+            if (rolId == null)
+                return false;
+
+            return rolesPermitidos.Contains(rolId.Value);
+        }
+
+
+        protected bool EsAdmin()
+        {
+            var rol = HttpContext.Session.GetInt32("RolId");
+            return rol == 1; // 1 = Administrador
+        }
+
+    }
+}
